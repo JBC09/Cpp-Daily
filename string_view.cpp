ccc5f599006ea81e77a19a628ca116c3c25867bd
정찬빈 <chanbin5634@clonix.co.kr>
2025-11-10 11:30:01 +0900

    add: string_view.cpp

---
+++ string_view.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+void f_ref(const std::string& s) // 단순히 상수 참조로 가져옴
+{
+    std::cout << s << "\n";
+}
+
+void f_view(std::string_view sv) // String_view를 사용하여 가져옴
+{
+    std::cout << sv << "\n";
+}
+
+
+int string_viewTest()
+{
+    std::string s =  "Alice";
+    f_ref(s);
+    f_ref("Bob"); // 상수 참조일 경우에는 가능함 (lifeTime을 연장해주기 떄문임)
+
+    f_view(s);
+    f_view("Bob");
+    f_view(std::string_view("Key=value").substr(0, 3));
+
+    // std::string_view sv bad;
+    //
+    // {
+    //     std::string temp = "tmp";
+    //     bad = temp; // temp가 파괴된다면 Bad는 댕글링 상태가 됌
+    // }
+}
